de17463009920baf0860ea8d37ae0be5022f8f2f
Benny Dreamly <bgaming0277@gmail.com>
2026-07-27 20:46:00 -0600

    match memorybase constructor

--- include/scene/memoryCard/iplMemoryCardBase.h
+++ include/scene/memoryCard/iplMemoryCardBase.h
@@ -5,7 +5,7 @@
 
 namespace ipl {
     namespace scene {
-        SCENE_CLASS(MemoryBase) {
+        class MemoryBase {
             public:
 
             struct AnmName {
@@ -18,6 +18,16 @@ namespace ipl {
 
             // Must be virtual to match vtable offset 0x30!
             virtual void add_animation(const AnmName* anims, int count);
+
+            private:
+
+            u32 mField_0x04;            // +0x04
+            u32 mField_0x08;            // +0x08
+            u32 mUnkPad_0x0C;           // +0x0C
+
+            nw4r::ut::List mList_0x10;  // +0x10
+            nw4r::ut::List mList_0x1C;  // +0x1C
+            nw4r::ut::List mList_0x28;  // +0x28
         };
     }
 }
