e1d50d9fbd80061dc0088c7fbfe26e34b9fb1792
Benny Dreamly <bgaming0277@gmail.com>
2026-07-27 19:24:16 -0600

    Start decompiling GCSaveData (this is rough)

---
+++ include/scene/memoryCard/iplGCSaveData.h
@@ -0,0 +1,14 @@
+#ifndef IPL_SCENE_GC_SAVE_DATA_H
+#define IPL_SCENE_GC_SAVE_DATA_H
+
+#include "iplSceneHeader.h"
+
+namespace ipl {
+    namespace scene {
+        SCENE_CLASS(GCSaveData);
+        GCSaveData(EGG::Heap * heap, nand::LayoutFile * param_3, const char * param_4, const char* param_5, math::VEC3 param_6);
+
+    }
+}
+
+#endif  // IPL_SCENE_GC_SAVE_DATA_H
---
+++ include/scene/memoryCard/iplMemoryCardBase.h
@@ -0,0 +1,11 @@
+#ifndef IPL_SCENE_MEMORY_CARD_BASE_H
+#define IPL_SCENE_MEMORY_CARD_BASE_H
+
+#include "iplSceneHeader.h"
+
+namespace ipl {
+    namespace scene {
+        SCENE_CLASS()
+
+    }
+}
